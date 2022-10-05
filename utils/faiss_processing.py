29452f099f0405773c472a33bc0fd7f4c5e392c4
nhattuong <tuongdotn@gmail.com>
2022-10-05 09:53:43 +0700

    Update faiss_processing.py

--- utils/faiss_processing.py
+++ utils/faiss_processing.py
@@ -28,15 +28,18 @@ class File4Faiss():
         image_paths = sorted(glob.glob(f'{video_path}/*.jpg'))
         for im_path in image_paths:
           # im_path = '/'.join(im_path.split('/')[-3:])
-          files.append(im_path)     
+          files.append(im_path)   
+
     id2img_fps = dict(enumerate(files))
     
     with open(des_path, 'w') as f:
       f.write(json.dumps(id2img_fps))
+
     print(f'Saved {des_path}')
 
   def write_bin_file(self, bin_path: str, method='L2', feature_shape=512):
     feature_paths = sorted(glob.glob(f'{self.root_database}/CLIPFeatures_C0*_V00'))
+
     if method in 'L2':
       index = faiss.IndexFlatL2(feature_shape)
     elif method in 'cosine':
@@ -53,6 +56,7 @@ class File4Faiss():
           index.add(feat)  
     
     faiss.write_index(index, os.path.join(bin_path, f"faiss_{method}.bin"))
+
     print(f'Saved {os.path.join(bin_path, f"faiss_{method}.bin")}')
 
 class MyFaiss():
@@ -67,6 +71,7 @@ class MyFaiss():
   def load_json_file(self, json_path: str):
       with open(json_path, 'r') as f:
         js = json.loads(f.read())
+
       return {int(k):v for k,v in js.items()}
 
   def load_bin_file(self, bin_file: str):
@@ -76,38 +81,46 @@ class MyFaiss():
     fig = plt.figure(figsize=(15, 10))
     columns = int(math.sqrt(len(image_paths)))
     rows = int(np.ceil(len(image_paths)/columns))
+
     for i in range(1, columns*rows +1):
       img = plt.imread(image_paths[i - 1])
       ax = fig.add_subplot(rows, columns, i)
       ax.set_title('/'.join(image_paths[i - 1].split('/')[-3:]))
+
       plt.imshow(img)
       plt.axis("off")
+      
     plt.show()
 
-  def __call__(self, id_query, k=9):    
+  def image_search(self, id_query, k=9):    
     query_feats = self.index.reconstruct(id_query).reshape(1,-1)
+
     scores, idx_image = self.index.search(query_feats, k=k)
     idx_image = idx_image.flatten()
+
     image_paths = list(map(self.id2img_fps.get, list(idx_image)))
     
     # print(f"scores: {scores}")
     # print(f"idx: {idx_image}")
     # print(f"paths: {image_paths}")
+    
     return scores, idx_image, image_paths
 
-  def predict(self, text, k=9):
+  def text_search(self, text, k=9):
     text = self.translater(text)
-    print(f'Translation: {text}')
+
     text = clip.tokenize([text]).to(self.__device)  
     text_features = self.model.encode_text(text).cpu().detach().numpy().astype(np.float32)
 
     scores, idx_image = self.index.search(text_features, k=k)
     idx_image = idx_image.flatten()
+
     image_paths = list(map(self.id2img_fps.get, list(idx_image)))
     
     # print(f"scores: {scores}")
     # print(f"idx: {idx_image}")
     # print(f"paths: {image_paths}")
+
     return scores, idx_image, image_paths
 
 def main():
@@ -117,11 +130,12 @@ def main():
 
   bin_file='./faiss_cosine.bin'
   json_path = './keyframes_id.json'
+
   cosine_faiss = MyFaiss('./Database', bin_file, json_path)
 
   #### Testing ####
   text = 'trận bóng đá Việt Nam'
-  scores, _, image_paths = cosine_faiss.predict(text, k=9)
+  scores, _, image_paths = cosine_faiss.text_search(text, k=9)
   cosine_faiss.show_images(image_paths)
 
 if __name__ == "__main__":
