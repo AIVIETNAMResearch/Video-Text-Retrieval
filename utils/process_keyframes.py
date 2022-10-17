8f7b27509aeb967173664ae4c5f7aad38dbbe1fd
nhattuong <tuongdotn@gmail.com>
2022-10-17 17:34:28 +0700

    Upload process_keyframes.py

---
+++ utils/process_keyframes.py
@@ -0,0 +1,15 @@
+import os
+import cv2 
+import glob
+
+def resize_keyframes(Database_path):
+    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+    img_paths = glob.glob(f'{Database_path}/KeyFrames*/*/*.jpg')
+
+    for img_path in img_paths:
+        img = cv2.imread(img_path)
+        img = cv2.resize(img, (224,224))
+
+        os.system(f'rm {img_path}')
+        cv2.imwrite(img_path, img)
