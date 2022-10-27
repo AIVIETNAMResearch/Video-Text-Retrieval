1db6ad77423876017b0d83de16b31525bfac8c22
DESKTOP-PCPA7E5\tuann <tuanngoc06102000@gmail.com>
2022-10-27 16:26:01 +0700

    Update

--- utils/ocr_processing.py
+++ utils/ocr_processing.py
@@ -1,17 +1,31 @@
 import rapidfuzz
 from rapidfuzz import process, fuzz
 import pandas as pd 
+import json
+
+FRAMEPATH_2_ID = json.load(open('dict\keyframe_path2id.json','r'))
 
 def get_top_ocr(str_query, df_ocr, k=200):  
-  results = df_ocr.apply(lambda x: fuzz.token_sort_ratio(str_query, str(x['text'])), axis=1)
-  df_ocr['results'] = results
+  df_ocr['results'] = df_ocr['text'].apply(lambda x: fuzz.token_sort_ratio(str_query, str(x)))
   results_top= df_ocr.sort_values(by='results', ascending=False).head(k)
 
-  return results_top
+  data = {}
+  for i in results_top.index:
+    row = results_top.loc[i]
+    video_id = row.video_id
+    frame_id = row.keyframe_id
+
+    frame_path = f'Database/KeyFrames{video_id[:-2]}/{video_id}/{frame_id:06}.jpg'
+    id_frame_path = FRAMEPATH_2_ID[frame_path]
+    
+    data[frame_path] = id_frame_path
+
+  return data
 
 if __name__ == "__main__":
     df_ocr = pd.read_csv('dict/ocr_results.txt')
-    df_ocr.columns = ["video_id ", "keyframe_id", "text"]
+    df_ocr.columns = ["video_id", "keyframe_id", "text"]
+    df_ocr.drop_duplicates(inplace=True)
 
     path_query = 'path_query'  #/content/query-pack-0/query-1.txt
     print(get_top_ocr(path_query, df_ocr))
